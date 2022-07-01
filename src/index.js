11c288549b05d3b3d6aaaa81f8e81630904c9a05
JoshuaHolloway <jhollow6@asu.edu>
2022-07-01 14:54:12 -0500

    svg

--- src/index.js
+++ src/index.js
@@ -17,20 +17,53 @@ function App() {
         <img className='logo' src={logo} alt='' />
         <div className='contaienr'>
           <div>
-            <img src={img1} alt='' />
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='24.074'
+              height='19.2'
+              viewBox='0 0 19.244 23.981'
+            >
+              <path
+                id='Shape'
+                d='M2.393,23.981A2.391,2.391,0,0,1,0,21.583L.012,2.4A2.391,2.391,0,0,1,2.406,0h9.622l7.216,7.195V21.583a2.4,2.4,0,0,1-2.406,2.4Z'
+                fill='#697ed4'
+              />
+            </svg>
           </div>
           <div>
-            <img src={img2} alt='' />
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='24.074'
+              height='19.2'
+              viewBox='0 0 24.074 19.2'
+            >
+              <path
+                id='Path'
+                d='M21.666,2.4h-9.63L9.629,0H2.407a2.393,2.393,0,0,0-2.4,2.4L0,16.8a2.4,2.4,0,0,0,2.407,2.4H21.666a2.4,2.4,0,0,0,2.407-2.4V4.8A2.4,2.4,0,0,0,21.666,2.4Z'
+                fill='#697ed4'
+              />
+            </svg>
           </div>
           <div>
-            <img src={img3} alt='' />
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='24.074'
+              height='19.2'
+              viewBox='0 0 24.074 16'
+            >
+              <path
+                id='Shape'
+                d='M6.018,16a6.015,6.015,0,0,1-.6-12,7.342,7.342,0,0,1,6.62-4A7.544,7.544,0,0,1,19.46,6a5.1,5.1,0,0,1,4.614,5,4.96,4.96,0,0,1-5.016,5Z'
+                fill='#697ed4'
+              />
+            </svg>
           </div>
         </div>
       </div>
       <div className='B'>
         <p>You’ve used 815 GB of your storage</p>
         <div className='bar-container'>
-          <svg></svg>
+          {/* <svg></svg> */}
           <p>0 GB</p>
           <p>1000 GB</p>
         </div>
